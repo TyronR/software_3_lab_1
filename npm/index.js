b48507a79f8683b813c99ac5aaf7a132e8de4e57
tyron reddy <869409@students.wits.ac.za>
2019-03-02 20:42:17 +0200

    change port bug

--- npm/index.js
+++ npm/index.js
@@ -11,5 +11,7 @@ let app = express();
 // res.sendFile(path.join(__dirname, 'views', 'about.html'));
 // });
 app.use(mainRouter);
-app.listen(3000);
-console.log(`Express server running on port 3000`);
+let port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Express server running on port", port);
+
