194aabf53ff3475154568bc02e2652d5f0c6b348
tyron reddy <869409@students.wits.ac.za>
2019-03-02 20:48:50 +0200

    change js file locations

---
+++ index.js
@@ -0,0 +1,17 @@
+let mainRouter = require('./mainRoutes')
+let path = require('path');
+let express = require('express');
+let app = express();
+// let mainRouter = express.Router();
+
+// mainRouter.get('/', function (req, res) {
+// res.send('Hello World');
+// });
+// mainRouter.get('/about', function(req, res){
+// res.sendFile(path.join(__dirname, 'views', 'about.html'));
+// });
+app.use(mainRouter);
+let port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Express server running on port", port);
+
